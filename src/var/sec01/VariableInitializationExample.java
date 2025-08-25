55bd2934f7d8a4b37f93e91ce7523d0e107bc4ac
beulrakbi <iamjyh99@gmail.com>
2025-08-25 12:38:42 +0900

    날짜와 시간 & 형식화

--- src/var/sec01/VariableInitializationExample.java
+++ src/var/sec01/VariableInitializationExample.java
@@ -13,7 +13,7 @@ public class VariableInitializationExample {
     * */
     public static void main(String[] args) {
 
-        int value; //변수 선언
+        //int value; //변수 선언
         int value1 = 10;
      //연산 결과를 변수 result의 초기값으로 대입
      //int result = value + 10; // value 변수를 초기화 하지 않고 사용할 수 없음
