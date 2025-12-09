015853f30f2442f0a054cf9c311d29341d9322ea
beulrakbi <iamjyh99@gmail.com>
2025-12-09 13:42:30 +0900

    Operator

---
+++ src/operator/Logical1.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class Logical1 {
+    public static void main(String[] args) {
+        System.out.println("&&: AND 연산");
+        System.out.println(true && true);
+        System.out.println(true && false);
+
+        System.out.println("||: OR 연산");
+        System.out.println(true || true);
+        System.out.println(true || false);
+
+        System.out.println("!연산");
+        boolean a = true;
+        boolean b = false;
+        System.out.println(!true);
+        System.out.println(!false);
+        System.out.println(a && b);
+    }
+}
---
+++ src/operator/ex/Operation3.java
@@ -0,0 +1,9 @@
+package operator.ex;
+
+public class Operation3 {
+    public static void main(String[] args) {
+        int scroe = 80;
+        boolean result = scroe>=80 && scroe <= 100;
+        System.out.println("result = " + result);
+    }
+}
